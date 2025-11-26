53ab5113b6c2077fe8eba96a4d650ef5612e0780
Manan Mishra <146760182+MananHere576@users.noreply.github.com>
2025-11-26 11:58:03 +0530

    Time: 3 ms (90.59%), Space: 46 MB (19.46%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -1,13 +1,19 @@
-import java.util.Arrays;
-
 class Solution {
     public boolean isAnagram(String s, String t) {
-        char[] sChars = s.toCharArray();
-        char[] tChars = t.toCharArray();
-        
-        Arrays.sort(sChars);
-        Arrays.sort(tChars);
-        
-        return Arrays.equals(sChars, tChars);
+
+        char[] a = s.toCharArray();
+        char[] b = t.toCharArray();
+
+        Arrays.sort(a);
+        Arrays.sort(b);
+
+        String str = new String(a);
+        String gtr = new String(b);
+
+        if(str.equals(gtr))
+        {
+            return true;
+        }
+        return false;
     }
 }
