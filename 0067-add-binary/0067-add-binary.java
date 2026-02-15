0ed7910c8b52c6b26ea800d0af9ce79b09fa27ef
Manan Mishra <146760182+MananHere576@users.noreply.github.com>
2026-02-15 07:19:26 +0530

    Time: 3 ms (17.52%), Space: 45.9 MB (5.09%) - LeetHub

---
+++ 0067-add-binary/0067-add-binary.java
@@ -0,0 +1,25 @@
+class Solution {
+    public String addBinary(String a, String b) {
+        String str="";
+        int i=a.length()-1;
+        int j=b.length()-1;
+        int carry=0;
+        while(i>=0 || j>=0 || carry==1)
+        {
+            if(i>=0)
+            {
+                carry+=a.charAt(i--)-'0';
+              
+
+            }
+            if(j>=0)
+            {
+                carry+=b.charAt(j--)-'0';
+            }
+            str=String.valueOf(carry%2)+str;
+            carry/=2;
+
+        }
+        return str;
+    }
+}
